faef2f494fad1ae43d4f0fadb9661f6731c39f3f
Sami Khan <samik966@gmail.com>
2024-07-13 17:41:37 +0530

    Added AutoComplete Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,80 @@
 import { useState } from "react";
 import { BiUser } from "react-icons/bi";
+import AutoComplete from "./autocomplete";
 import CustomSelect from "./custom-select";
 
-// type Data = { key: string; label: string };
+type Data = { value: string; label: string };
 
+// const options1: string[] = ["Something 1", "Something 2", "Something 3"];
+
+const options2: Data[] = [
+    { value: "Something1", label: "Something 1" },
+    { value: "Something2", label: "Something 2" },
+    { value: "Something3", label: "Something 3" },
+];
 function App() {
-    const [selectedOption, setSelectedOption] = useState<string | null>(null);
+    const [selectedOption, setSelectedOption] = useState<Data | null>(null);
+    const [autocompleteValue, setAutoCompleteValue] = useState<string>("");
+    const [selectedOptions, setSelectedOptions] = useState<Data[]>([]);
 
-    // const _options: Data[] = [
-    //     { key: "Something1", label: "Something 1" },
-    //     { key: "Something2", label: "Something 2" },
-    //     { key: "Something3", label: "Something 3" },
-    // ];
-    const options2: string[] = ["Something 1", "Something 2", "Something 3"];
+    const handleSelect = (option: Data | Data[]) => {
+        if (Array.isArray(option)) {
+            setSelectedOptions(option);
+        } else {
+            setSelectedOption(option);
+        }
+    };
 
-    const handleSelect = (option: string) => {
-        setSelectedOption(option);
+    const handleChange = (newValue: string) => {
+        setAutoCompleteValue(newValue);
     };
 
     return (
         <div>
             <h1 className="text-4xl">UI Components</h1>
-            <div className="px-4">
-                <CustomSelect
-                    options={options2}
-                    // valueKey="key"
-                    // labelKey="label"
-                    value={selectedOption}
-                    onSelect={handleSelect}
-                    activeClassName="bg-red-300"
-                    selectClassName="border-red-400 focus-visible:outline-red-300"
-                    optionClassName="hover:bg-red-200"
-                    renderOption={(option) => (
-                        <div className="flex items-center gap-2">
-                            <BiUser />
-                            {option}
-                        </div>
-                    )}
-                />
+            <div className="container mx-auto my-4">
+                <div className="flex gap-4 border p-4">
+                    <CustomSelect
+                        options={options2}
+                        labelKey="label"
+                        valueKey="value"
+                        value={selectedOption}
+                        onSelect={handleSelect}
+                        renderOption={(option) => {
+                            return (
+                                <div className="flex items-center gap-1">
+                                    <BiUser />
+                                    {option.label}
+                                </div>
+                            );
+                        }}
+                    />
+                    <AutoComplete
+                        multiple={true}
+                        labelKey="label"
+                        valueKey="value"
+                        // limitPill={1}
+                        value={autocompleteValue}
+                        options={options2}
+                        selectedOptions={selectedOptions}
+                        onChange={handleChange}
+                        onSelect={handleSelect}
+                        tagClassName="bg-rose-400 text-white"
+                        containerClassName="border-rose-100 focus-within:ring-rose-300 focus-visible:outline-rose-200"
+                        optionClassName="hover:bg-rose-100"
+                        activeClassName="bg-rose-300"
+                        renderOption={(option) => {
+                            return (
+                                <div className="flex items-center gap-1">
+                                    <BiUser />
+                                    {option.label}
+                                </div>
+                            );
+                        }}
+                        renderNoOption={() => <div>No Type Found</div>}
+                    />
+                </div>
             </div>
-            <p>Selected Option: {selectedOption}</p>
         </div>
     );
 }
--- src/custom-select/index.tsx
+++ src/custom-select/index.tsx
@@ -1,166 +1,40 @@
-import { CSSProperties, KeyboardEvent, MouseEvent as RMouseEvent, useRef, useState } from "react";
+import { useRef } from "react";
 import { MdKeyboardArrowDown, MdKeyboardArrowUp } from "react-icons/md";
 import useClickOutside from "../hooks/useClickOutside";
+import useOptions from "../hooks/useOptions";
+import Options, { OptionProps } from "../Options";
 import { cn } from "../utils/lib";
+import { getOptionKeyOrValue } from "../utils/options-helper";
 
-interface CustomSelectProps<T> {
+interface CustomSelectProps<T> extends OptionProps<T> {
     id?: string;
-    options: T[];
-    value: T | null;
-    labelKey?: keyof T;
-    valueKey?: keyof T;
-    activeClassName?: string;
-    optionClassName?: string;
     popupClassName?: string;
     selectClassName?: string;
-    onSelect: (option: T) => void;
-    renderOption?: (option: T, index: number) => JSX.Element;
 }
 
 function CustomSelect<T>({
     options,
     value,
     onSelect,
-    activeClassName = "bg-indigo-300",
+    activeClassName,
     id = "select-label",
     ...rest
 }: CustomSelectProps<T>) {
-    const [isOpen, setIsOpen] = useState(false);
-    const [focusedIndex, setFocusedIndex] = useState(-1);
-    const listRef = useRef<HTMLUListElement>(null);
     const selectRef = useRef<HTMLDivElement>(null);
-
-    const onOptionSelect = (e: RMouseEvent<HTMLUListElement>) => {
-        const liElement = (e.target as HTMLElement).closest("li[data-index]");
-        if (liElement) {
-            const index = liElement.getAttribute("data-index");
-            if (index !== null) {
-                onSelect(options[Number(index)]);
-            }
-        }
-        setIsOpen(false);
-    };
-
-    const isSelected = (option: T) => {
-        if (option) {
-            return getOptionValue(value) === getOptionValue(option);
-        }
-        return false;
-    };
-
-    const getOptionLabel = (option: T | null) => {
-        if (!option) return "";
-        if (typeof option === "object" && rest.labelKey) {
-            return option[rest?.labelKey] as string;
-        }
-        return option as string;
-    };
-
-    const getOptionValue = (option: T | null) => {
-        if (!option) return "";
-        if (typeof option === "object" && rest.valueKey) {
-            return option[rest.valueKey];
-        }
-        return option;
-    };
-
-    const renderOptionsList = () => {
-        return options.map((opt, idx) => {
-            const selected = isSelected(opt);
-            return (
-                <li
-                    role="option"
-                    aria-selected={selected}
-                    key={idx}
-                    tabIndex={-1}
-                    className={cn(
-                        "cursor-pointer select-none px-2 py-1 hover:bg-indigo-100",
-                        idx !== options.length - 1 && "border-b",
-                        selected && activeClassName,
-                        focusedIndex === idx && activeClassName,
-                        rest.optionClassName,
-                    )}
-                    style={
-                        { "--tw-bg-opacity": focusedIndex === idx ? "0.25" : "1" } as CSSProperties
-                    }
-                    data-index={idx}
-                >
-                    {rest.renderOption ? (
-                        rest.renderOption(opt, idx)
-                    ) : (
-                        <span> {getOptionLabel(opt)}</span>
-                    )}
-                </li>
-            );
-        });
-    };
-
-    const handleOpenToggle = () => {
-        if (!document.startViewTransition) {
-            setIsOpen((prev) => !prev);
-        }
-        document.startViewTransition(() => {
-            setIsOpen((prev) => !prev);
-        });
-    };
-
-    const handleOpen = () => {
-        if (!document.startViewTransition) {
-            setIsOpen(true);
-        }
-        document.startViewTransition(() => {
-            setIsOpen(true);
-        });
-    };
-    const handleClose = () => {
-        if (!document.startViewTransition) {
-            setIsOpen(false);
-        }
-        document.startViewTransition(() => {
-            setIsOpen(false);
-        });
-    };
-
-    const handleSelectKeyDown = (e: KeyboardEvent<HTMLDivElement>) => {
-        switch (e.code) {
-            case "Enter":
-                e.preventDefault();
-                onSelect(options[focusedIndex]);
-                handleClose();
-                break;
-            case "Escape":
-                e.preventDefault();
-                handleClose();
-                break;
-            case "Space":
-                e.preventDefault();
-                handleOpen();
-                break;
-            case "ArrowDown":
-                e.preventDefault();
-                setFocusedIndex((prev) => (prev + 1) % options.length);
-                break;
-            case "ArrowUp":
-                e.preventDefault();
-                setFocusedIndex((prev) => (prev - 1 + options.length) % options.length);
-                break;
-            case "Home":
-                e.preventDefault();
-                setFocusedIndex(0);
-                break;
-            case "End":
-                e.preventDefault();
-                setFocusedIndex(options.length - 1);
-                break;
-            default:
-                break;
-        }
-    };
+    const { isOpen, focusedIndex, handleClose, handleKeyDown, handleOpenToggle } = useOptions({
+        options,
+        onSelect,
+    });
 
     useClickOutside(selectRef, handleClose);
 
+    const handleSelect = (selectedValue: T) => {
+        onSelect(selectedValue);
+        handleClose();
+    };
+
     return (
-        <div ref={selectRef} className="relative w-auto min-w-40 max-w-64">
+        <div ref={selectRef} className="relative">
             <div
                 role="combobox"
                 aria-expanded={isOpen}
@@ -168,32 +42,27 @@ function CustomSelect<T>({
                 aria-labelledby={id}
                 tabIndex={0}
                 className={cn(
+                    "w-auto min-w-40 max-w-64",
                     "cursor-pointer rounded border border-indigo-100 focus-visible:outline-indigo-200",
                     rest.selectClassName,
                 )}
-                onKeyDown={handleSelectKeyDown}
+                onKeyDown={handleKeyDown}
                 onClick={handleOpenToggle}
             >
                 <div id={id} className="flex select-none items-center justify-between px-2">
-                    <span>{getOptionLabel(value) || "Select Value"}</span>
+                    <span>{getOptionKeyOrValue(value, rest.labelKey) || "Select Value"}</span>
                     {isOpen ? <MdKeyboardArrowUp /> : <MdKeyboardArrowDown />}
                 </div>
             </div>
             {isOpen && (
-                <div className={"relative inset-0 top-full mt-0.5 max-h-40 w-full overflow-y-auto"}>
-                    <ul
-                        ref={listRef}
-                        role="listbox"
-                        aria-activedescendant={`option-${focusedIndex}`}
-                        className={cn(
-                            "rounded border border-indigo-200 bg-white shadow-sm",
-                            rest.popupClassName,
-                        )}
-                        onClick={onOptionSelect}
-                    >
-                        {renderOptionsList()}
-                    </ul>
-                </div>
+                <Options
+                    value={value}
+                    focusedIndex={focusedIndex}
+                    options={options}
+                    onSelect={handleSelect}
+                    activeClassName={activeClassName}
+                    {...rest}
+                />
             )}
         </div>
     );
